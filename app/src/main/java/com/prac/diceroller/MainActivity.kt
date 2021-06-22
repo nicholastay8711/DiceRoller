892095b291c59449ce8f55cec47e7857e7acb8ff
Nicholas <taywjn-wa16@student.tarc.edu.my>
2021-06-22 16:14:36 +0800

    part 3

--- app/src/main/java/com/prac/diceroller/MainActivity.kt
+++ app/src/main/java/com/prac/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.prac.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -10,8 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val resultText: TextView= findViewById(R.id.result_text)
-        resultText.text="0"
+        val diceImage: ImageView= findViewById(R.id.dice_image)
+        diceImage.setImageResource(R.drawable.empty_dice)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
@@ -19,8 +20,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView= findViewById(R.id.result_text)
-        val randomInt =(1..6).random()
-        resultText.text=randomInt.toString()
+        val diceImage: ImageView= findViewById(R.id.dice_image)
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt){
+            1 ->R.drawable.dice_1
+            2 ->R.drawable.dice_2
+            3 ->R.drawable.dice_3
+            4 ->R.drawable.dice_4
+            5 ->R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
     }
 }
